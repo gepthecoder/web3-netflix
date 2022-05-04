5ac189e4054dd0886040f1bb3dedf3467ee7d1ad
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-04 11:25:59 +0200

    $0008: [HomePage]: create exportable movies library

---
+++ Netflix-Starter/src/helpers/library.js
@@ -0,0 +1,20 @@
+import titanicS from "../images/titanicScene.png";
+import tita from "../images/tit.png";
+import movieLogo from "../images/movieLogo.png";
+
+export const movies =
+[
+     {
+        Movie: "",
+        Thumnbnail: tita,
+        Scene: titanicS,
+        Logo: movieLogo,
+        Name: "Titanic",
+        Description: 
+        "James Cameron's Titanic is an epic, action-packed romance set against the ill-fated maiden voyage of the R.M.S. Titanic; the pride and joy of the White Star Line and, at the time, the largest moving object ever built. She was the most luxurious liner of her era -- the ship of dreams -- which ultimately carried over 1,500 people to their death in the ice cold waters of the North Atlantic in the early hours of April 15, 1912.",
+        Genre: "Romance, Drama",
+        Actors: "Kate Winslet, Leonardo DiCaprio",
+        Year: "1998",
+        Duration: "3h 14min",
+    },
+];
