c3d90d5ad5fd5a6279135bd49469fc7e59cce967
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-04 10:48:06 +0200

    $0003: [TopBanner]: add logo and style

--- Netflix-Starter/src/pages/Home.js
+++ Netflix-Starter/src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import "./Home.css";
+import { Logo } from '../images/Netflix';
 
 const Home = () => {
 
 return(
   <>
+  <div className="logo">
+    <Logo />
+  </div>
   <div className="container">
      <Link to="/player" className="link">Player Page</Link>
   </div>
