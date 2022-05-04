e9ef2ec1b7c7193cdb07b1a52313731aacd91c28
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-04 12:57:54 +0200

    $0018: [MoviePlayer]: show movie player and auto play selected movie + style

--- Netflix-Starter/src/pages/Player.js
+++ Netflix-Starter/src/pages/Player.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useLocation } from "react-router";
 
 const Player = () => {
+
+  const {state: currentlyPlaying} = useLocation();
   
   return (
   <>
-  <div className="container">
-  <Link to="/" className="link">Home</Link>
+  <div className="playerPage">
+    <video autoPlay controls className="videoPlayer">
+      <source
+        src={currentlyPlaying}
+        type="video/mp4"
+      >
+      </source>
+    </video>
   </div>
+  {currentlyPlaying}
   </>
 )
 }
